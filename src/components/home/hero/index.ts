0e5a227fb36bd705465e86c34b3b40d9434ce0c0
Elmas Okic <hi@eokic.com>
2023-08-03 14:39:57 +0200

    Component restructuring; `Hero.astro` is now segmented

---
+++ src/components/home/hero/index.ts
@@ -0,0 +1,7 @@
+// PARTIALS
+export { default as Aside } from './_Aside.astro'
+export { default as Clients } from './_Clients.astro'
+export { default as Main } from './_Main.astro'
+
+// COMPONENTS
+export { default as Hero } from './Hero.astro'
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,1 @@
-export { default as HomeHero } from './HomeHero.astro'
+export * from './home'
