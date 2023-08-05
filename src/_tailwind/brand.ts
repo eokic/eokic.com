4540e10a2bb50c5c51448fafdd0e7e1afff13a0f
Elmas Okic <hi@eokic.com>
2023-08-05 17:52:34 +0200

    Accessibility improvements for `Button` and `Link` components

--- src/_tailwind/brand.ts
+++ src/_tailwind/brand.ts
@@ -5,6 +5,7 @@ export default {
     primary: 'hsl(var(--color-secondary) / 1)',
     secondary: 'hsl(var(--color-primary) / 1)',
     tertiary: 'hsl(var(--color-tertiary) / 1)',
+    'focus-sr': 'hsl(var(--color-focus-sr) / 1)',
   },
 
   font: {
