2f66b31f22f8dcd8d5712624741ee0d4c195a698
Elmas Okic <hi@eokic.com>
2023-08-06 13:00:56 +0200

    Implements a basic sticky `Header`

--- src/assets/main.ts
+++ src/assets/main.ts
@@ -1,22 +1,44 @@
 import Motus from 'motus'
 
-const newAnimation = new Motus.Animation({
-  $el: document.getElementById('hero-photo'),
+// Get all the elements
+// TODO: check WHEN does this script get executed in production
+const header = document.getElementById('header')
+const heroDesc = document.getElementById('hero-desc')
+const heroPhoto = document.getElementById('hero-photo')
+
+
+/* --------------------------------------------------------------------------
+  STICKY HEADER
+-------------------------------------------------------------------------- */
+new IntersectionObserver(e => {
+  if (e[0]?.intersectionRatio <= 0)
+    header.classList.add('show')
+  else
+    header.classList.remove('show')
+}, {
+  threshold: [0, 0.25, 0.5, 0.75, 1],
+}).observe(heroDesc)
+
+
+/* --------------------------------------------------------------------------
+  Motus parallax animations
+-------------------------------------------------------------------------- */
+const heroPhotoParallax = new Motus.Animation({
+  $el: heroPhoto,
   startPoint: 0,
   endPoint: 600,
   keyframes: [
     { translateY: -100 },
   ],
 })
-Motus.addAnimation(newAnimation)
+Motus.addAnimation(heroPhotoParallax)
 
-
-const newAnimation2 = new Motus.Animation({
-  $el: document.getElementById('hero-desc'),
+const heroDescParallax = new Motus.Animation({
+  $el: heroDesc,
   startPoint: 0,
   endPoint: 500,
   keyframes: [
     { translateY: 8 },
   ],
 })
-Motus.addAnimation(newAnimation2)
+Motus.addAnimation(heroDescParallax)
