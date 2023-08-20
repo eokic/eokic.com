14267ae22ce1d3313e21b0c7abfdda65312aa243
Elmas Okic <hi@eokic.com>
2023-08-20 21:20:32 +0200

    Updates the `Link` types; adds `footerLinks` to the `Global` type

--- src/interfaces/Global.ts
+++ src/interfaces/Global.ts
@@ -1,7 +1,8 @@
-import type { IEntity, ILogo, ISEO } from '.'
+import type { IEntity, ILink, ILogo, ISEO } from '.'
 
 export default interface IHome extends IEntity {
   logo: ILogo
   footer?: string
+  footerLinks?: ILink[]
   seo: ISEO
 }
--- src/interfaces/_Link.ts
+++ src/interfaces/_Link.ts
@@ -1,5 +1,7 @@
 export default interface ILink {
   url: string
+  title?: string
   isPrivate?: boolean // defaults to TRUE
   openInNewTab?: boolean // defaults to null
+  icon?: 'github' | 'linkedin' | null
 }
